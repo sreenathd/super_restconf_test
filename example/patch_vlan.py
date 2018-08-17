73f91e7e73b2add14c39788f65cb258141730cdf
Sreenath D <sreenathd2002@gmail.com>
2018-08-17 22:10:57 +0530

    Update patch_vlan.py

--- example/patch_vlan.py
+++ example/patch_vlan.py
@@ -7,3 +7,4 @@ headers = {
 data = '{"config":{"name":"lab-test-vlan33"},"vlan-id":33}'
 
 response = requests.patch('http://172.31.57.16:8538/restconf/data/running/openconfig-vlan:vlans/vlan=33/config/vlan-id', headers=headers, data=data, auth=('ADMIN', 'ADMIN'))
+print (response.status_code)
