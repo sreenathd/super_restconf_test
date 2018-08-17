f77f5a0a068de1165c04d640de93feea5699f96d
Sreenath D <sreenathd2002@gmail.com>
2018-08-17 22:18:18 +0530

    Update creat_vlan.py

--- example/creat_vlan.py
+++ example/creat_vlan.py
@@ -6,4 +6,4 @@ headers = {
 
 data = '{"vlan-id":33}'
 
-response = requests.post('http://172.32.57.16:8538/restconf/data/running/openconfig-vlan:vlans', headers=headers, data=data, auth=('ADMIN', 'ADMIN'))
+response = requests.post('http://172.31.57.16:8538/restconf/data/running/openconfig-vlan:vlans', headers=headers, data=data, auth=('ADMIN', 'ADMIN'))
