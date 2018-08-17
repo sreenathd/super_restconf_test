72878d6ce95c0975fada51f1ab4d6d2f65f3726c
Sreenath D <sreenathd2002@gmail.com>
2018-08-17 22:39:04 +0530

    Update RestConf.py

--- RestConf.py
+++ RestConf.py
@@ -115,6 +115,9 @@ def patch(uri='',data=None):
         'Content-Type': 'application/yang-data+json',
     }
     #data = '{"config":{"name":"lab-test-vlan33"},"vlan-id":33}'
-    resp = requests.patch('http://172.31.57.16:8538/restconf/data/running/openconfig-vlan:vlans/vlan=33/config/vlan-id', headers=headers, data=data, auth=('ADMIN', 'ADMIN'))
+    if 'vlan' in data:
+        resp = requests.patch('http://172.31.57.16:8538/restconf/data/running/openconfig-vlan:vlans/vlan=33/config/vlan-id', headers=headers, data=data, auth=('ADMIN', 'ADMIN'))
+    else:
+        resp = rest_session.patch(data,uri)
     return resp
 
