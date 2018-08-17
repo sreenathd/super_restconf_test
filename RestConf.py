231df2466237ce1aec8ec37b3bf4897f474912bf
Sreenath D <sreenathd2002@gmail.com>
2018-08-17 22:33:19 +0530

    Update RestConf.py

--- RestConf.py
+++ RestConf.py
@@ -95,7 +95,7 @@ def delete(uri=''):
         'Content-Type': 'application/yang-data+json',
     }
     #data = '{"config":{"name":"lab-test-vlan33"},"vlan-id":33}'
-    resp = requests.delete('http://172.31.57.16:8538/restconf/data/running/openconfig-vlan:vlans/vlan=33/config/vlan-id', headers=headers, auth=('ADMIN', 'ADMIN'))
+    resp = requests.delete('http://172.31.57.16:8538/restconf/data/running/openconfig-vlan:vlans/vlan=33', headers=headers, auth=('ADMIN', 'ADMIN'))
     return resp
 
 def post(uri='',data=None):
