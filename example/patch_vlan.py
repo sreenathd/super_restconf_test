c4d6ef0eb1bfe8a3af84b7e4c56edb6ab708d7e5
Sreenath D <sreenathd2002@gmail.com>
2018-08-17 22:10:23 +0530

    Create patch_vlan.py

---
+++ example/patch_vlan.py
@@ -0,0 +1,9 @@
+import requests
+
+headers = {
+    'Content-Type': 'application/yang-data+json',
+}
+
+data = '{"config":{"name":"lab-test-vlan33"},"vlan-id":33}'
+
+response = requests.patch('http://172.31.57.16:8538/restconf/data/running/openconfig-vlan:vlans/vlan=33/config/vlan-id', headers=headers, data=data, auth=('ADMIN', 'ADMIN'))
