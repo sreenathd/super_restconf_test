df8164282e3fa0a2c5e2c903cb1b4202f09260f1
Sreenath D <sreenathd2002@gmail.com>
2018-08-17 06:35:16 +0530

    Create creat_vlan.py

---
+++ example/creat_vlan.py
@@ -0,0 +1,9 @@
+import requests
+
+headers = {
+    'Content-Type': 'application/yang-data+json',
+}
+
+data = '{"vlan-id":33}'
+
+response = requests.post('http://172.32.57.16:8538/restconf/data/running/openconfig-vlan:vlans', headers=headers, data=data, auth=('ADMIN', 'ADMIN'))
